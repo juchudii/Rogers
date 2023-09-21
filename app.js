11f6aaafa1c66854f2cab1b9b6055aec8668ec42
juchudii <145682634+juchudii@users.noreply.github.com>
2023-09-21 08:25:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_1jgxcxa", "template_upt5m86", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
